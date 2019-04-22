f577b06c8fa06c0593d89e4af5f97085f48cf272
ArmanHZ <43811481+ArmanHZ@users.noreply.github.com>
2019-04-22 18:11:28 +0300

    Fixed some problems about fops

--- storage_driver.c
+++ storage_driver.c
@@ -1,98 +1,127 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/usb.h>
+#include <linux/cdev.h>
 #include <linux/fs.h>
-#include <linux/semaphore.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 
-#define DEVICE_NAME "USB Storage"
-
-static struct usb_driver storage_driver = {
-  .name = "USB Storage Driver",
-  .id_table = storage_table,
-  .probe = storage_probe,
-  .disconnect = storage_disconnect,
-};
+#define MIN(a,b) (((a) <= (b)) ? (a) : (b))
+#define BULK_EP_OUT 0x01
+#define BULK_EP_IN 0x82
+#define MAX_PKT_SIZE 512
 
 static struct usb_device *device;
-static struct cdev *my_cdev;
-dev_t device_number; // Number that kernel will give us
+static struct usb_class_driver class;
+static unsigned char bulk_buf[MAX_PKT_SIZE];
 
-// usb_device_id from kernel using command lsusb
-static struct usb_device_id storage_table[] = {
-  // Vendor id: 058f  Product id: 6387
-  { USB_DEVICE(0x058f, 0x6387) },
-  {}  // Terminating entry
+static int storage_open(struct inode *i, struct file *f) {
+    return 0;
+}
+
+static int storage_close(struct inode *i, struct file *f) {
+    return 0;
+}
+
+static ssize_t storage_read(struct file *f, char __user *buf, size_t cnt, loff_t *off) {
+    int retval;
+    int read_cnt;
+
+    /* Read the data from the bulk endpoint */
+    retval = usb_bulk_msg(device, usb_rcvbulkpipe(device, BULK_EP_IN), bulk_buf, MAX_PKT_SIZE, &read_cnt, 5000);
+    if (retval) {
+        printk(KERN_ERR "Bulk message returned %d\n", retval);
+        return retval;
+    }
+    if (copy_to_user(buf, bulk_buf, MIN(cnt, read_cnt))) {
+        return -EFAULT;
+    }
+
+    return MIN(cnt, read_cnt);
+}
+
+static ssize_t storage_write(struct file *f, const char __user *buf, size_t cnt, loff_t *off) {
+    int retval;
+    int wrote_cnt = MIN(cnt, MAX_PKT_SIZE);
+
+    if (copy_from_user(bulk_buf, buf, MIN(cnt, MAX_PKT_SIZE))) {
+        return -EFAULT;
+    }
+
+    /* Write the data into the bulk endpoint */
+    retval = usb_bulk_msg(device, usb_sndbulkpipe(device, BULK_EP_OUT), bulk_buf, MIN(cnt, MAX_PKT_SIZE), &wrote_cnt, 5000);
+    if (retval) {
+        printk(KERN_ERR "Bulk message returned %d\n", retval);
+        return retval;
+    }
+
+    return wrote_cnt;
+}
+
+static struct file_operations fops = {
+    .owner = THIS_MODULE,
+    .open = storage_open,
+    .release = storage_close,
+    .read = storage_read,
+    .write = storage_write,
 };
 
-// Registerin the device with the system
-// Creating a device file in the process
-static int driver_entry(void) {
-  int return_value = alloc_chrdev_region(&device_number, 0, 1, DEVICE_NAME);
-  if (return_value < 0) {
-    printk(KERN_ALLERT "Failed to allocate a major number");
-    return return_value;
-  }
-  int major_number = MAJOR(device_number);
-  printk(KERN_INFO "Storage USB: major number is %d", major_number);
-  printk(KERN_INFO "\tuse \t"mknod /dev/%s c %d 0\" for device file", DEVICE_NAME, major_number);
+static int storage_probe(struct usb_interface *interface, const struct usb_device_id *id)
+{
+    int retval;
 
-  my_cdev = cdev_alloc();
-  my_cdev->ops = &fops;
-  my_cdev->owner = THIS_MODULE;
-  return_value = cdev_add(my_cdev, dev_num, 1);
-  if (return_value < 0) {
-    printk(KERN_ALLERT "Unable to add Character device to kernel");
-    return return_value;
-  }
-  sema_init(&storage_driver.sem, 1);
+    struct usb_host_interface *iface_desc;
+    struct usb_endpoint_descriptor *endpoint;
 
-  return 0;
+    iface_desc = interface->cur_altsetting;
+    printk(KERN_INFO "Storage USB interface %d now probed: (%04X:%04X)\n", iface_desc->desc.bInterfaceNumber, id->idVendor, id->idProduct);
+    printk(KERN_INFO "ID->bNumEndpoints: %02X\n", iface_desc->desc.bNumEndpoints);
+    printk(KERN_INFO "ID->bInterfaceClass: %02X\n", iface_desc->desc.bInterfaceClass);
+
+    int i;
+    for (i = 0; i < iface_desc->desc.bNumEndpoints; i++) {
+        endpoint = &iface_desc->endpoint[i].desc;
+        printk(KERN_INFO "ED[%d]->bEndpointAddress: 0x%02X\n", i, endpoint->bEndpointAddress);
+        printk(KERN_INFO "ED[%d]->bmAttributes: 0x%02X\n", i, endpoint->bmAttributes);
+        printk(KERN_INFO "ED[%d]->wMaxPacketSize: 0x%04X (%d)\n", i, endpoint->wMaxPacketSize, endpoint->wMaxPacketSize);
+    }
+    device = interface_to_usbdev(interface);
+    class.name = "usb/storage_driver%d";
+    class.fops = &fops;
+    if ((retval = usb_register_dev(interface, &class)) < 0) {
+        printk(KERN_ERR "Not able to get a minor for this device.");
+    } else {
+        printk(KERN_INFO "Minor obtained: %d\n", interface->minor);
+    }
+
+    printk(KERN_INFO "*** USB Probed ***");
+    return retval;
 }
 
-static void driver_exit(void) {
-  cdev_del(my_cdev);
-  unregister_chrdev_region(device_number, 1);
-  printk(KERN_ALLERT "Unloaded module");
+static void storage_disconnect(struct usb_interface *interface) {
+    printk(KERN_INFO "*** USB Disconnected ***");
+    usb_deregister_dev(interface, &class);
 }
 
-// Probe function: Called when USB is inserted to the PC
-// Also prints info about the USB to kernel. (dmesg)
-static int storage_probe(struct usb_interface *interface, const struct usb_device_id *id) {
-  struct usb_host_interface *host_interface;
-  struct usb_endpoint_descriptor *endpoint_descriptor;
+static struct usb_device_id storage_table[] = {
+    { USB_DEVICE(0x058F, 0x6387) },
+    {} /* Terminating entry */
+};
 
-  host_interface = interface->cur_altsetting;
+MODULE_DEVICE_TABLE (usb, storage_table);
 
-  printk(KERN_INFO "*** USB Storage Probed ***");
-  printk(KERN_INFO "USB Storage with interface no: %d, vendor id: %04X, product id: %04X, is now Probbed\n",
-    host_interface->desc.bInterfaceNumber, id->idVendor, id->idProduct);
-  printk(KERN_INFO "Number of Endpoints: %02X\n", host_interface->desc.bInterfaceNumber);
-  printk(KERN_INFO "Interface Class id: %02X\n", host_interface->desc.bInterfaceClass);
-
-  int i;
-  for (i = 0; i < host_interface->desc.bNumEndpoints; i++) {
-      endpoint_descriptor = &host_interface->endpoint[i].desc;
-      printk(KERN_INFO "ED[%d]: Endpoint Address: 0x%02X\n", i, endpoint_descriptor->bEndpointAddress);
-      printk(KERN_INFO "ED[%d]: bmAttributes: 0x%02X\n", i, endpoint_descriptor->bmAttributes);
-      printk(KERN_INFO "ED[%d]: Max Packet Size: 0x%04X (%d)\n", i, endpoint_descriptor->wMaxPacketSize, endpoint_descriptor->wMaxPacketSize);
-  }
-  device = interface_to_usbdev(interface);
-  return 0;
-}
-
-static void storage_disconnect(struct usb_interface* interface) {
-  printk(KERN_INFO "*** USB Storage is removed ***");
-}
-
+static struct usb_driver storage_driver = {
+    .name = "storage_driver",
+    .probe = storage_probe,
+    .disconnect = storage_disconnect,
+    .id_table = storage_table,
+};
 
 static int __init storage_init(void) {
-  printk(KERN_INFO "*** USB Storage Init function ***");
-  return usb_register(&storage_driver);
+    return usb_register(&storage_driver);
 }
 
 static void __exit storage_exit(void) {
-  usb_deregister(&storage_driver);
+    usb_deregister(&storage_driver);
 }
 
 module_init(storage_init);
