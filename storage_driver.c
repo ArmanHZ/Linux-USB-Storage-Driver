6702d0875730a3962d4ce62fae8bf415d3984558
ArmanHZ <43811481+ArmanHZ@users.noreply.github.com>
2019-04-22 16:46:41 +0300

    Added Device file creation

--- storage_driver.c
+++ storage_driver.c
@@ -5,8 +5,18 @@
 #include <linux/semaphore.h>
 #include <asm/uaccess.h>
 
+#define DEVICE_NAME "USB Storage"
+
+static struct usb_driver storage_driver = {
+  .name = "USB Storage Driver",
+  .id_table = storage_table,
+  .probe = storage_probe,
+  .disconnect = storage_disconnect,
+};
 
 static struct usb_device *device;
+static struct cdev *my_cdev;
+dev_t device_number; // Number that kernel will give us
 
 // usb_device_id from kernel using command lsusb
 static struct usb_device_id storage_table[] = {
@@ -15,6 +25,37 @@ static struct usb_device_id storage_table[] = {
   {}  // Terminating entry
 };
 
+// Registerin the device with the system
+// Creating a device file in the process
+static int driver_entry(void) {
+  int return_value = alloc_chrdev_region(&device_number, 0, 1, DEVICE_NAME);
+  if (return_value < 0) {
+    printk(KERN_ALLERT "Failed to allocate a major number");
+    return return_value;
+  }
+  int major_number = MAJOR(device_number);
+  printk(KERN_INFO "Storage USB: major number is %d", major_number);
+  printk(KERN_INFO "\tuse \t"mknod /dev/%s c %d 0\" for device file", DEVICE_NAME, major_number);
+
+  my_cdev = cdev_alloc();
+  my_cdev->ops = &fops;
+  my_cdev->owner = THIS_MODULE;
+  return_value = cdev_add(my_cdev, dev_num, 1);
+  if (return_value < 0) {
+    printk(KERN_ALLERT "Unable to add Character device to kernel");
+    return return_value;
+  }
+  sema_init(&storage_driver.sem, 1);
+
+  return 0;
+}
+
+static void driver_exit(void) {
+  cdev_del(my_cdev);
+  unregister_chrdev_region(device_number, 1);
+  printk(KERN_ALLERT "Unloaded module");
+}
+
 // Probe function: Called when USB is inserted to the PC
 // Also prints info about the USB to kernel. (dmesg)
 static int storage_probe(struct usb_interface *interface, const struct usb_device_id *id) {
@@ -22,9 +63,10 @@ static int storage_probe(struct usb_interface *interface, const struct usb_devic
   struct usb_endpoint_descriptor *endpoint_descriptor;
 
   host_interface = interface->cur_altsetting;
+
+  printk(KERN_INFO "*** USB Storage Probed ***");
   printk(KERN_INFO "USB Storage with interface no: %d, vendor id: %04X, product id: %04X, is now Probbed\n",
     host_interface->desc.bInterfaceNumber, id->idVendor, id->idProduct);
-
   printk(KERN_INFO "Number of Endpoints: %02X\n", host_interface->desc.bInterfaceNumber);
   printk(KERN_INFO "Interface Class id: %02X\n", host_interface->desc.bInterfaceClass);
 
@@ -40,17 +82,12 @@ static int storage_probe(struct usb_interface *interface, const struct usb_devic
 }
 
 static void storage_disconnect(struct usb_interface* interface) {
-  printk(KERN_INFO "USB Storage is removed");
+  printk(KERN_INFO "*** USB Storage is removed ***");
 }
 
-static struct usb_driver storage_driver = {
-  .name = "USB Storage Driver",
-  .id_table = storage_table,
-  .probe = storage_probe,
-  .disconnect = storage_disconnect,
-};
 
 static int __init storage_init(void) {
+  printk(KERN_INFO "*** USB Storage Init function ***");
   return usb_register(&storage_driver);
 }
 
